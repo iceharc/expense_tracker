dfa7135d66e23b1a0b6789d34e513ff885b337fe
iceharc <159436477+iceharc@users.noreply.github.com>
2026-04-02 12:27:08 +0300

    Delete myapp directory
    not needed

--- myapp/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY=os.getenv("SECRET_KEY",'supersecret')
-    SQLALCHEMY_DATABASE_URI="sqlite:///expenses.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-    JWT_SECRET_KEY=os.getenv("JWT_SECRET_KEY","jwtsecret")
